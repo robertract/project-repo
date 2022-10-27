5b4d4c94f8a7a843aa0e4fe997820ce08514c58f
Robert Frederico <robertract@hotmail.com>
2022-10-26 22:05:19 -0300

    Inicializando repositório
    Inicializando repositório

---
+++ src/pages/Main/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Form, SubmitButton } from './styles'
+import { FaGithub, FaPlus } from 'react-icons/fa';
+
+export default function Main() {
+    return (
+        <Container>
+            <h1>
+                <FaGithub size={25} />
+                Meus Repositórios
+            </h1>
+
+            <Form onSubmit={() => { }}>
+                <input type="text" placeholder="Adicionar Repositórios" />
+
+                <SubmitButton>
+                    <FaPlus color="#FFF" size={14} />
+                </SubmitButton>
+            </Form>
+        </Container>
+    )
+}
